83920a8ec82ad6ed8b4a60c13ab9c74428ecf0fe
David <deivid.prz3412@gmail.com>
2025-08-26 12:19:50 -0400

    sorting para tablas por id de forma ascendente

--- frontend/panaderia-interfaz/src/features/Recetas/components/RecetasTablerows.tsx
+++ frontend/panaderia-interfaz/src/features/Recetas/components/RecetasTablerows.tsx
@@ -2,6 +2,7 @@ import { RecetaTablerow } from "./RecetaTablerow";
 import type { recetaItem } from "../types/types";
 
 export const RecetasTablerows = ({ data }: { data: recetaItem[] }) => {
+  data = data.sort((a,b) => a.id - b.id);
   return (
     <>
       {data.map((item, index) => (
