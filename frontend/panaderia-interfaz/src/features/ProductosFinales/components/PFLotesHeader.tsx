17378e88dc15e9b9ce66c5e3ba2e8e857c0bb8c0
David <deivid.prz3412@gmail.com>
2025-10-23 14:28:24 -0400

    change to lotes table for products and prioritize the display of available items first on the table

--- frontend/panaderia-interfaz/src/features/ProductosFinales/components/PFLotesHeader.tsx
+++ frontend/panaderia-interfaz/src/features/ProductosFinales/components/PFLotesHeader.tsx
@@ -1,6 +1,6 @@
 export const PFLotesHeader = () => {
     return (
-        <div className="p-4 grid grid-cols-6 font-bold font-[Roboto] text-sm">
+        <div className="p-4 grid grid-cols-6 font-bold font-[Roboto] text-sm bg-[var(--table-header-bg)] rounded-t-md">
             <div>Stock Inicial</div>
             <div>Stock actual</div>
             <div>Fecha de caducidad</div>
