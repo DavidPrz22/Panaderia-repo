041025b5ad18d356db2e2ae0332d326b07e4f4dc
David <deivid.prz3412@gmail.com>
2025-12-31 14:55:44 -0400

    change range date pricker language from english to spanish

--- frontend/panaderia-interfaz/src/components/RangeDatePicker.tsx
+++ frontend/panaderia-interfaz/src/components/RangeDatePicker.tsx
@@ -1,5 +1,6 @@
 import { Calendar } from "@/components/ui/calendar";
 import { type DateRange } from "react-day-picker"
+import { es } from "date-fns/locale"
 
 export const RangeDatePicker = ({ selected, onSelect }: { selected: DateRange | undefined, onSelect: (selected: DateRange | undefined) => void }) => {
 
@@ -9,6 +10,7 @@ export const RangeDatePicker = ({ selected, onSelect }: { selected: DateRange | 
             selected={selected}
             onSelect={onSelect}
             numberOfMonths={2}
+            locale={es}
         />
     )
 }
